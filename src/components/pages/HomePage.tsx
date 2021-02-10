4b42527eeed00efb7c52889171f364055a46e505
cfm <419081990@qq.com>
2021-02-10 09:05:50 +0800

    1

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import * as echarts from 'echarts';
 
+import imgbk from 'style/img/bxy.png';
+
 class Main extends Component {
     componentDidMount() {
         let myChart = echarts.init(document.getElementById('main') as HTMLDivElement);
@@ -12,29 +14,26 @@ class Main extends Component {
                 r: 40
             },
             style: {
-                fill: 'none',
-                stroke: '#F00'
+                fill: 'red',
             },
             onmouseover: () => {
-                console.log(circle.getPaintRect().x);
             },
             draggable: true,
         });
         zr.add(circle);
-        circle.animate("shape",true);
+
+        let img = new Image();
+        img.src = imgbk;
+
+        myChart.getRenderedCanvas({backgroundColor: "blue"}) as HTMLCanvasElement;
+        img.onload = () =>{
+        }
     }
     render() {
         return (
             <div className="vrv-home">
                 <div id="main" className="vrv-home-main">
-                    {/* <canvas id="myCanvas" className="vrv-home-main-can">
-                        Your browser does not support the canvas element.
-                        <div id="123">
-                            asdf
-                        </div>
-                    </canvas> */}
                 </div>
-
             </div>
         );
     }
